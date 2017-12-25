023b32303e64dce7497a040c32de004f204e7df2
Pontus Bystedt <ponbys@MAC049.local>
2017-12-25 23:29:17 +0100

    Add some styling

--- index.php
+++ index.php
@@ -3,9 +3,12 @@
 	* TODO:
 	*
 	* Do some mobile first styling
+	* CSS variables
+	* Separate CSS file into components
 	* Filter on already present cars with JS (especially bad in demo mode since demo cars times are random)
 	* Cleanup getcars
 	* Clear get params on in-date-input-clear
+	* Prevent downloading possibility of ini file
 	*/
 	include 'ParkingBot.php';
 
@@ -43,38 +46,40 @@
 <html>
 <head>
 	<title>Adeprimo Parking Bot</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-	<h1>Adeprimo - ParkingBot</h1>
-	<h4>Filter table</h4>
-	<form action="/" method="get" class="form">
-		From: <input type="date" name="filter_from" class="form-field" value="<?= $filter_from; ?>">
-		To: <input type="date" name="filter_to" class="form-field" value="<?= $filter_to; ?>">
-		<input type="submit" name="submit" class="form-submit" value="Filter">
-	</form>
-	<button class="button" onclick="window.location = '/';">Clear filter</button>
-	<table class="list">
-		<tr class="list-row">
-			<td class="list-row-item">License plate</td>
-			<td class="list-row-item">Check-in time</td>
-			<td class="list-row-item">Check-out time</td>
-			<td class="list-row-item" align="right">Parking fee in <?= $env['ticket_price_zone_a_currency'] ?></td>
-		</tr>
-		<?php foreach ($car_lists as $name => $cars): ?>
-			<?php foreach ($cars as $registration_number => $car): ?>
-				<?php $total_fee += $car['fee']; ?>
-				<tr class="list-row">
-					<td class="list-row-item"><?= $car['registration_number'] ?: $registration_number; ?></td>
-					<td class="list-row-item"><?= date('Y-m-d H:i', $car['start_time']); ?></td>
-					<td class="list-row-item"><?= $car['end_time'] ? date('Y-m-d H:i', $car['end_time']) : 'Still parked'; ?></td>
-					<td class="list-row-item" align="right"><?= $car['fee']; ?></td>
-				</tr>
+	<div class="main">
+		<h1 class="heading">Adeprimo - ParkingBot</h1>
+		<form action="/" method="get" class="form">
+			From: <input type="date" name="filter_from" class="form-field" value="<?= $filter_from; ?>">
+			To: <input type="date" name="filter_to" class="form-field" value="<?= $filter_to; ?>">
+			<input type="submit" name="submit" class="form-submit" value="Filter">
+		</form>
+		<button class="button" onclick="window.location = '/';">Clear filter</button>
+		<table class="list">
+			<tr class="list-row">
+				<td class="list-row-item">License plate</td>
+				<td class="list-row-item">Check-in time</td>
+				<td class="list-row-item">Check-out time</td>
+				<td class="list-row-item" align="right">Parking fee in <?= $env['ticket_price_zone_a_currency'] ?></td>
+			</tr>
+			<?php foreach ($car_lists as $name => $cars): ?>
+				<?php foreach ($cars as $registration_number => $car): ?>
+					<?php $total_fee += $car['fee']; ?>
+					<tr class="list-row">
+						<td class="list-row-item"><?= $car['registration_number'] ?: $registration_number; ?></td>
+						<td class="list-row-item"><?= date('Y-m-d H:i', $car['start_time']); ?></td>
+						<td class="list-row-item"><?= $car['end_time'] ? date('Y-m-d H:i', $car['end_time']) : 'Still parked'; ?></td>
+						<td class="list-row-item" align="right"><?= $car['fee']; ?></td>
+					</tr>
+				<?php endforeach; ?>
 			<?php endforeach; ?>
-		<?php endforeach; ?>
-		<tr class="list-row">
-			<td class="list-row-item" colspan="3"><?= count($car_lists['old_cars']) + count($car_lists['active_cars']); ?> cars in total</td>
-			<td class="list-row-item" align="right">Total amount: <?= $total_fee; ?></td>
-		</tr>
-	</table>
+			<tr class="list-row">
+				<td class="list-row-item" colspan="3"><?= count($car_lists['old_cars']) + count($car_lists['active_cars']); ?> cars in total</td>
+				<td class="list-row-item" align="right">Total amount: <?= $total_fee; ?></td>
+			</tr>
+		</table>
+	</div>
 </body>
 </html>
